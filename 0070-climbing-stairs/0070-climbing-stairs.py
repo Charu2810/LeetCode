f14149eefe22e8c597dca01921a410e44b22e470
Charu Lata <charulata2810@gmail.com>
2026-07-09 23:35:07 +0530

    Time: 0 ms (100%), Space: 12.5 MB (20.23%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def climbStairs(self, n):
+        if n==1:
+            return 1
+        prev1,prev2=2,1
+        for i in range(3,n+1):
+            current=prev1+prev2
+            prev2=prev1
+            prev1=current
+        return prev1
+        
