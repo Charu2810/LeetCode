846c41bd7b0ca460eefb474432d0d4773bed1d1b
Charu Lata <charulata2810@gmail.com>
2026-07-25 20:39:22 +0530

    Time: 0 ms (100%), Space: 12.5 MB (16.08%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProduct(self, n):
+        m = 0
+        while n:
+            m += 1 << ((n % 10) << 2)
+            n //= 10
+
+        u = (m.bit_length() - 1) >> 2
+        return u * (((m - (1 << (u << 2))).bit_length() - 1) >> 2)
