84371c8a09f1a7732e037581e89f5ac8153f0baa
Charu Lata <charulata2810@gmail.com>
2026-09-07 00:41:40 +0530

    Time: 5 ms (71.7%), Space: 31.2 MB (7.55%) - LeetHub

--- 1953-maximum-number-of-weeks-for-which-you-can-work/1953-maximum-number-of-weeks-for-which-you-can-work.py
+++ 1953-maximum-number-of-weeks-for-which-you-can-work/1953-maximum-number-of-weeks-for-which-you-can-work.py
@@ -1,8 +1,5 @@
 class Solution:
     def numberOfWeeks(self, milestones: List[int]) -> int:
-        mx = max(milestones)
-        sm = sum(milestones) - mx
-        if sm >= mx:
-            return sm+mx
-        else: 
-            return 2*sm+1 
+        total = sum(milestones)
+        maximum = max(milestones)
+        return min(total, 2 * (total - maximum) + 1)
